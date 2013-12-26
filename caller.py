94e2a95bf6eff52f276adc9f325afb8ed6193389
James <J1459.j1459@gmail.com>
2013-12-26 22:49:17 +0800

    Added update function Moved download code into function

--- caller.py
+++ caller.py
@@ -15,24 +15,41 @@ import subprocess
 import sys
 
 
+def update():
+    """Try to update youtube-dl.exe"""
+    print "Attempting to update...."
+    program_location = sys.executable
+    program_name = "youtube-dl.exe"
+    update_arg = "--update"
+    command = [program_name, update_arg]
+    result = subprocess.call(command)
+    print "Command result: ", result
+    print "Finished updating."
 
 
-program_location = sys.executable
-program_name = "youtube-dl.exe"
-ignore_errors = "-i"
-safe_filenames = "--restrict-filenames"
-output_arg = "-o"
-output_template = "%(uploader)s\%(playlist)s\%(title)s-%(id)s.%(ext)s"
-target_url = raw_input("Enter target URL")
+def download():
+    """Do downloads using youtube-dl.exe"""
+    program_location = sys.executable
+    program_name = "youtube-dl.exe"
+    ignore_errors = "-i"
+    safe_filenames = "--restrict-filenames"
+    output_arg = "-o"
+    output_template = "download\%(uploader)s\%(playlist)s\%(title)s-%(id)s.%(ext)s"
+    target_url = None
+    while target_url is not "":
+        target_url = raw_input("Enter target URL")
+        command = [program_name, ignore_errors, safe_filenames, output_arg, output_template, target_url]
+        result = subprocess.call(command)
+        print "Command result: ", result
+        print ""
+        print ""
 
-command = [program_name, ignore_errors, safe_filenames, output_arg, output_template, target_url]
-result = subprocess.call(command)
-print "Command result: ", result
 
-raw_input("Press return to exit.")
 
 def main():
-    pass
+    update()
+    download()
+    raw_input("Press return to exit.")
 
 if __name__ == '__main__':
     main()
