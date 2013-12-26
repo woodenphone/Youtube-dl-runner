37bf3efb52efa5c127c84fe56d10e0f695d1909b
James <J1459.j1459@gmail.com>
2013-12-26 22:09:13 +0800

    Tools for working with youtube-dl Initial commit.

---
+++ caller.py
@@ -0,0 +1,38 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      new
+#
+# Created:     26/12/2013
+# Copyright:   (c) new 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+
+import subprocess
+import sys
+
+
+
+
+program_location = sys.executable
+program_name = "youtube-dl.exe"
+ignore_errors = "-i"
+safe_filenames = "--restrict-filenames"
+output_arg = "-o"
+output_template = "%(uploader)s\%(playlist)s\%(title)s-%(id)s.%(ext)s"
+target_url = "https://www.youtube.com/playlist?list=PL-XXv-cvA_iBTE0TyTr1MXaoihYpqPhwc"
+
+command = [program_name, ignore_errors, safe_filenames, output_arg, output_template, target_url]
+result = subprocess.call(command)
+print "Command result: ", result
+
+raw_input("Press return to exit.")
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
