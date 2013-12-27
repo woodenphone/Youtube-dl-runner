6fe601bbea69b298a041b934cbb788af06a06961
James <J1459.j1459@gmail.com>
2013-12-27 09:59:12 +0800

    Added code to read clipboard. Moved ui to seperate function. Added delay after updating to fix problem where called program printed text after script asked for input.

--- caller.py
+++ caller.py
@@ -13,6 +13,17 @@
 
 import subprocess
 import sys
+import time
+import Tkinter
+
+
+def read_clipboard():
+    # http://stackoverflow.com/questions/7881230/copy-data-from-the-clipboard-on-linux-mac-and-windows-with-a-single-python-scri?rq=1
+    root = Tkinter.Tk()
+    root.withdraw() # Hide the main window (optional)
+    text_in_clipboard = root.clipboard_get()
+    return text_in_clipboard
+
 
 
 def update():
@@ -24,31 +35,47 @@ def update():
     command = [program_name, update_arg]
     result = subprocess.call(command)
     print "Command result: ", result
+    time.sleep(5)
     print "Finished updating."
 
 
-def download():
+def download(target_url):
     """Do downloads using youtube-dl.exe"""
     program_location = sys.executable
     program_name = "youtube-dl.exe"
+    # Define arguments. see this url for help
+    # https://github.com/rg3/youtube-dl
     ignore_errors = "-i"
     safe_filenames = "--restrict-filenames"
     output_arg = "-o"
     output_template = "download\%(uploader)s\%(playlist)s\%(title)s-%(id)s.%(ext)s"
-    target_url = None
-    while target_url is not "":
-        target_url = raw_input("Enter target URL")
-        command = [program_name, ignore_errors, safe_filenames, output_arg, output_template, target_url]
-        result = subprocess.call(command)
-        print "Command result: ", result
-        print ""
-        print ""
+    command = [program_name, ignore_errors, safe_filenames, output_arg, output_template, target_url]
+    result = subprocess.call(command)
+    print "Command result: ", result
 
+
+
+
+
+
+def user_interface():
+    while True:
+        print ""
+        print ""
+        print "Enter target URL"
+        print "Leave blank to read clipboard. Enter x to exit."
+        target_url = raw_input("")
+        if target_url == "":
+            target_url = read_clipboard()
+            print "Read from clipboard:", target_url
+        elif target_url == "x":
+            return
+        download(target_url)
 
 
 def main():
     update()
-    download()
+    user_interface()
     raw_input("Press return to exit.")
 
 if __name__ == '__main__':
